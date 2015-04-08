e689cf5001b141278f55dccdc8907f4cb68f5d02
fa18swiss <fa18swiss@users.noreply.github.com>
2015-04-08 18:47:45 +0200

    FileZilla + winscp

--- EasyRemote.ProgramsProtocols/Programs/FileZilla.cs
+++ EasyRemote.ProgramsProtocols/Programs/FileZilla.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EasyRemote.Impl.Extension;
 using EasyRemote.ProgramsProtocols.Protocols;
 using EasyRemote.Spec;
 
@@ -6,15 +9,41 @@ namespace EasyRemote.ProgramsProtocols.Programs
 {
     class FileZilla : _Base
     {
-        public FileZilla(SSH ssh, FTP ftp, FTPS ftps)
-            : base("FileZilla", ssh, ftp, ftps)
+         private readonly IDictionary<IProtocol, string> protocolsDictionnary;
+         public FileZilla(SSH ssh, FTP ftp, FTPS ftps)
+             : base("FileZilla", ssh, ftp, ftps)
         {
-            
+            protocolsDictionnary = new Dictionary<IProtocol, string>();
+            protocolsDictionnary.Add(ssh, "sftp");
+            protocolsDictionnary.Add(ftp, "ftp");
+            protocolsDictionnary.Add(ftps, "ftps");
         }
 
         public override string ConnectTo(IServer server, IServerProtocol protocol)
         {
-            throw new NotImplementedException();
+            var builder = new StringBuilder();
+            builder.Append(protocolsDictionnary[protocol.Protocol]);
+            builder.Append("://");
+
+            var user = server.GetUsernameForProtocol(protocol);
+            if (!string.IsNullOrEmpty(user))
+            {
+                builder.Append(user);
+                var pwd = server.GetPasswordForProtocol(protocol);
+                if (!string.IsNullOrEmpty(pwd))
+                {
+                    builder.Append(':');
+                    builder.Append(pwd);
+                }
+                builder.Append('@');
+            }
+            builder.Append(server.HostName);
+            if (protocol.Port.HasValue)
+            {
+                builder.Append(':');
+                builder.Append(protocol.Port.Value);
+            }
+            return builder.ToString();
         }
     }
 }
