70f25fac2fe6ab8f3b6fd0286d4115738913b64c
fa18swiss <fa18swiss@users.noreply.github.com>
2015-04-08 17:35:09 +0200

    add rdp protocol

---
+++ EasyRemote.ProgramsProtocols/Programs/RDP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EasyRemote.ProgramsProtocols.Protocols;
+using EasyRemote.Spec;
+
+namespace EasyRemote.ProgramsProtocols.Programs
+{
+    class RDP : _Base
+    {
+        public RDP(Protocols.RDP rdp)
+            : base("RDP", rdp)
+        {
+            
+        }
+
+        public override void ConnectTo(IServer server, IServerProtocol protocol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EasyRemote.ProgramsProtocols/Protocols/RDP.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyRemote.ProgramsProtocols.Protocols
+{
+    class RDP : _Base
+    {
+        public RDP() 
+            : base("RDP", -1)
+        {
+            // TODO RDP port
+        }
+    }
+}
