79daf67d2c6d914e52caf1286ff5ab5a9d8dc84d
fa18swiss <fa18swiss@users.noreply.github.com>
2015-04-08 17:35:30 +0200

    Load protocols and programs from namespace

--- EasyRemote.ProgramsProtocols/Module.cs
+++ EasyRemote.ProgramsProtocols/Module.cs
@@ -1,5 +1,6 @@
-﻿using EasyRemote.ProgramsProtocols.Programs;
-using EasyRemote.ProgramsProtocols.Protocols;
+﻿using System.Linq;
+using System.Reflection;
+using EasyRemote.ProgramsProtocols.Util;
 using EasyRemote.Spec;
 using EasyRemote.Spec.Module;
 using Microsoft.Practices.Unity;
@@ -10,18 +11,15 @@ namespace EasyRemote.ProgramsProtocols
     {
         public void Load(IUnityContainer container)
         {
-            container.RegisterType<IProtocol, FTP>("FTP", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, FTPS>("FTPS", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, HTTP>("HTTP", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, HTTPS>("HTTPS", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, SSH>("SSH", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, Telnet>("Telnet", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProtocol, VNC>("VNC", new ContainerControlledLifetimeManager());
-
-            container.RegisterType<IProgram, FileZilla>("FileZilla", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProgram, Putty>("Putty", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProgram, TurboVNC>("TurboVNC", new ContainerControlledLifetimeManager());
-            container.RegisterType<IProgram, WinSCP>("WinSCP", new ContainerControlledLifetimeManager());
+            var assembly = Assembly.GetExecutingAssembly();
+            foreach (var type in assembly.GetTypesInNamespace(GetType().Namespace + ".Protocols").Where(t => !t.IsAbstract))
+            {
+                container.RegisterType(typeof (IProtocol), type, type.Name, new ContainerControlledLifetimeManager());
+            }
+            foreach (var type in assembly.GetTypesInNamespace(GetType().Namespace + ".Programs").Where(t => !t.IsAbstract))
+            {
+                container.RegisterType(typeof(IProgram), type, type.Name, new ContainerControlledLifetimeManager());
+            }
         }
     }
 }
---
+++ EasyRemote.ProgramsProtocols/Util/AssemblyUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace EasyRemote.ProgramsProtocols.Util
+{
+    public static class AssemblyUtil
+    {
+        public static IEnumerable<Type> GetTypesInNamespace(this Assembly assembly, string nameSpace)
+        {
+            return assembly.GetTypes().Where(t => String.Equals(t.Namespace, nameSpace, StringComparison.Ordinal));
+        }
+    }
+}
